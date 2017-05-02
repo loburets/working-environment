a9a4550de6f7d4d780ea9f73a569ca400f797f26
Dmitriy Loburets <dima-loburec@yandex.ru>
2017-05-02 16:08:26 +0600

    Added vhosts example for native mac lamp

---
+++ mac/vhosts.sh
@@ -0,0 +1,24 @@
+        ## CHANGE THIS DOMAINS
+        DOMAINS=(
+            "test.loc"
+        )
+
+        ## Loop through all sites
+        for ((i=0; i < ${#DOMAINS[@]}; i++)); do
+
+            ## Current Domain
+            DOMAIN=${DOMAINS[$i]}
+
+            echo "Creating directory for $DOMAIN..."
+            sudo mkdir -p /Users/loburec/Desktop/projects/$DOMAIN/public
+
+            echo "Creating vhost config for $DOMAIN..."
+            sudo cp /private/etc/apache2/extra/vhosts/example.conf /private/etc/apache2/extra/vhosts/$DOMAIN.conf
+
+            echo "Updating vhost config for $DOMAIN..."
+            #replace "example.loc" to $DOMAIN
+            sudo sed -i '' -e  "s/example.loc/$DOMAIN/g" /private/etc/apache2/extra/vhosts/$DOMAIN.conf
+        done
+
+        echo "So let's restart apache..."
+        sudo apachectl restart
